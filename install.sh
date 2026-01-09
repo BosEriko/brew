82b7c356ce702ec22d628657b4c5b0f0591374e3
Bos Eriko Reyes <bos@eriko.ph>
2026-01-09 19:44:02 +0800

    Add source link for Brave browser in install.sh
    Added source link for Brave browser installation.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ brew install --cask visual-studio-code      # Visual Studio Code (https://github
 brew install --cask karabiner-elements      # Karabiner-Elements (https://github.com/BosEriko/karabiner)
 
 # Install Utilities
-brew install --cask brave-browser           # Brave
+brew install --cask brave-browser           # Brave (https://github.com/BosEriko/brave)
 
 # Install Stream Tools
 brew install --cask obs                     # OBS (https://github.com/BosEriko/obs)
