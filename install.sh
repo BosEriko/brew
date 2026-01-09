f80db45f11b33c92189703419447b70b32d8c409
Bos Eriko Reyes <bos@eriko.ph>
2026-01-09 19:42:34 +0800

    Add installation commands for various applications
    Added installation commands for various applications including Visual Studio Code, Brave browser, OBS, Steam, and VLC.

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 # Install Work Tools
+brew install --cask visual-studio-code      # Visual Studio Code (https://github.com/BosEriko/vs)
 brew install --cask karabiner-elements      # Karabiner-Elements (https://github.com/BosEriko/karabiner)
+
+# Install Utilities
+brew install --cask brave-browser           # Brave
+
+# Install Stream Tools
+brew install --cask obs                     # OBS (https://github.com/BosEriko/obs)
+
+# Entertainment
+brew install --cask steam                   # Steam
+brew install --cask vlc                     # VLC
