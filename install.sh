8cb588f645692ca87a2018f818d06776151dc1eb
Bos Eriko Reyes <bos@eriko.ph>
2026-01-26 11:59:03 +0800

    Update install script to include Figma
    Added installation command for Figma and removed Brave browser installation.

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # Install Work Tools
-brew install --cask visual-studio-code      # Visual Studio Code (https://github.com/BosEriko/vs)
 brew install --cask karabiner-elements      # Karabiner-Elements (https://github.com/BosEriko/karabiner)
-
-# Install Utilities
-brew install --cask brave-browser           # Brave (https://github.com/BosEriko/brave)
+brew install --cask figma                   # Figma
 
 # Install Stream Tools
 brew install --cask obs                     # OBS (https://github.com/BosEriko/obs)
