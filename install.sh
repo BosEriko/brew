28c0975b86e383bdb063568f310f8b296ed7a121
Bos Eriko Reyes <bos@eriko.ph>
2026-01-27 20:14:41 +0800

    add obsidian

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 brew install --cask karabiner-elements      # Karabiner-Elements (https://github.com/BosEriko/karabiner)
 brew install --cask figma                   # Figma
 brew install --cask blender                 # Blender
+brew install --cask obsidian                # Obsidian
 
 # Install Stream Tools
 brew install --cask obs                     # OBS (https://github.com/BosEriko/obs)
