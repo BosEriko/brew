5ef96dc98f7fe893b42c75b4df7da165242e23dd
Bos Eriko Reyes <bos@eriko.ph>
2026-01-30 09:15:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 # Install Work Tools
 brew install --cask karabiner-elements      # Karabiner-Elements (https://github.com/BosEriko/karabiner)
 brew install --cask figma                   # Figma
-brew install --cask blender                 # Blender
-brew install --cask obsidian                # Obsidian
+brew install --cask postman                 # Postman
 
 # Install Stream Tools
 brew install --cask obs                     # OBS (https://github.com/BosEriko/obs)
